19d7bb46c888584f3d6e85944822b4841819760c
NICK VALLEE <nickvallee@gmail.com>
2017-02-13 11:26:08 -0500

    feat: new york times articles display based on location

--- js/script.js
+++ js/script.js
@@ -23,7 +23,34 @@ function loadData() {
     var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + street + '&' + location + '&key=' + streetViewKey;
 
 
+
+
     $body.append('<img class="bgimg" src="' + streetViewURL + '">');
+
+
+    var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    nytUrl += '?' + $.param({
+        'api-key': "24c783fdface474fb022155f11637cb7",
+        'q': city
+    });
+
+    $.getJSON( nytUrl, function( data ) {
+
+        $nytHeaderElem.text('New York Times Articles About ' + city);
+
+    articles = data.response.docs;
+    for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class="article">' +
+            '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+            '<p>' + article.snippet + '</p>'+
+            '</li>');
+    }
+
+    console.log(data);
+
+
+    });
 
 
     return false;
