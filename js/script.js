9b0b3ff1bf5a886404825196ff01b656264afcd8
NICK VALLEE <nickvallee@gmail.com>
2017-02-13 12:31:23 -0500

    feat: wikipedia links from the API display

--- js/script.js
+++ js/script.js
@@ -22,12 +22,9 @@ function loadData() {
 
     var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + street + '&' + location + '&key=' + streetViewKey;
 
-
-
-
     $body.append('<img class="bgimg" src="' + streetViewURL + '">');
 
-
+    /// NEW YORK TIMES API
     var nytUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     nytUrl += '?' + $.param({
         'api-key': "24c783fdface474fb022155f11637cb7",
@@ -45,11 +42,28 @@ function loadData() {
             '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
             '<p>' + article.snippet + '</p>'+
             '</li>');
-    }
-
-    console.log(data);
+    };
 
 
+    }).error(function(e) {
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    });
+
+    ///WIKIPEDIA API
+
+    var wikiLink='http://en.wikipedia.org/w/api.php?action=opensearch&search='+city+'&format=json';
+    console.log("wikiLink: "+wikiLink);
+
+    $.ajax(wikiLink, {
+        dataType: 'jsonp',
+    }).done(function (data) {
+        console.log(data);
+
+        for(i=0; i< data.length; i++) {
+            var wikiHeader = data[1][i];
+            var wikiArticleURL = data [3][i];
+            $wikiElem.append('<li><a href="' + wikiArticleURL+ '">' +wikiHeader+'</a>')
+        }
     });
 
 
